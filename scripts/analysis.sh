228628db87b2f35666aecb23cc5e2b97d07f3feb
Erik Saule <esaule@charlotte.edu>
2026-03-15 13:00:39 -0400

    analysis code

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-host=cci-hopper2 #probably take that off commandline at some point
+host=$1 #probably take that off commandline at some point
 
 
 resdir=../results/${host}/
@@ -16,7 +16,13 @@ gpu_bandwidth=$( awk -F \  '{if ($2 == 0){ print 2 * $1 *  4 / $3;}}'  < ${resdi
 
 interconnect_bandwidth=$(awk -F \  'BEGIN{maxv=0} {if ($2 == 0){bw = ($1 + $2 +1)*4*2 / $3; if (bw > maxv) {maxv = bw;}}} END{print maxv;}' < ${resdir}/gpu_stream )
 
-echo $peak_flops $gpu_bandwidth $interconnect_bandwidth
+uvm_bw=$(awk -F \  'BEGIN{maxv=0} {if ($2 == 0){bw = ($1 + $2 +1)*4*2 / $3; if (bw > maxv) {maxv = bw;}}} END{print maxv;}' < ${resdir}/gpu_uvm_basic)
+
+echo peak_flops $peak_flops
+echo gpu_bw $gpu_bandwidth
+echo interconnect_bw $interconnect_bandwidth
+echo uvm_bw $uvm_bw
+
 
 
 
